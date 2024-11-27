5c2bc7e91a578596fafe5a8673f637be2c829f69
Tatsuya Yano <tatyano@yahoo-corp.jp>
2024-11-27 18:55:47 +0900

    Changed configuration structure for certificate extra san dns

--- pkg/config/derived-service-cert.go
+++ pkg/config/derived-service-cert.go
@@ -35,8 +35,9 @@ type LocalCertMode struct {
 }
 
 type DerivedServiceCert struct {
-	CopperArgos CopperArgosMode
-	LocalCert   LocalCertMode // Use 3rd party provided service cert instead of CopperArgos
+	CopperArgos      CopperArgosMode
+	LocalCert        LocalCertMode // Use 3rd party provided service cert instead of CopperArgos
+	CertExtraSANDNSs []string
 }
 
 // derivedServiceCertConfig reads given configuration and sets the derived state of preparing service cert under the follow modes:
@@ -53,7 +54,8 @@ func (idCfg *IdentityConfig) derivedServiceCertConfig() error {
 			AthenzDomainName:  "",
 			AthenzServiceName: "",
 		},
-		LocalCert: LocalCertMode{Use: false},
+		LocalCert:        LocalCertMode{Use: false},
+		CertExtraSANDNSs: []string{},
 	}
 
 	if idCfg.providerService != "" {
